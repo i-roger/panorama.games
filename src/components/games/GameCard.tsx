a382c2ef4f24c34a39afb5af4782912258f16633
i-roger <mroger.dev@gmail.com>
2026-05-09 10:26:50 -0300

    First Commit!

--- src/components/games/GameCard.tsx
+++ src/components/games/GameCard.tsx
@@ -1,6 +1,6 @@
 import { Card } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
-import { Heart, Star } from 'lucide-react'
+import { Heart } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import type { Game } from '@/types'
 
